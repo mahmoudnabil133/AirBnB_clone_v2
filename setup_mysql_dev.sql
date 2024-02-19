9d4f65f4645e5cfb2102a1449fb6e405e0bfd97f
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-19 12:56:21 -0800

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- set up mysql for storage
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance schima.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES
+GRANT SELECT ON `performance schima`.* TO 'hbnb_dev'@'localhost';
