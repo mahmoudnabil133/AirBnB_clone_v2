c6245b99a01a6c5fe6cbf04068e783dc66e55373
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-20 13:09:51 -0800

    amenity

--- models/place.py
+++ models/place.py
@@ -5,11 +5,11 @@ from models.base_model import *
 place_amenity = Table('place_amenity', Base.metadata,
                      Column('place_id', String(60),
                             ForeignKey('places.id'),
-                            primary_key=True
+                            primary_key=True,
                             nullable=False),
                      Column('amenity_id', String(60),
                             ForeignKey('amenities.id'),
-                            primary_key=True
+                            primary_key=True,
                             nullable=False))
 
 
