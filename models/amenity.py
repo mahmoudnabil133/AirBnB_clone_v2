df1a15aeb4dd064e8ce4202014291c4ffe4fdc24
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-20 13:46:42 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -8,4 +8,4 @@ class Amenity(BaseModel, Base):
     """amenity class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenit = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship('Place', secondary='place_amenity')
--- models/place.py
+++ models/place.py
@@ -29,5 +29,5 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     reviews = relationship('Review', backref='place', cascade='delete')
     amenities = relationship('Amenity', secondary=place_amenity, viewonly=False,
-                             back_populates='place_amenit')
+                             back_populates='place_amenities')
     amenity_ids = []
