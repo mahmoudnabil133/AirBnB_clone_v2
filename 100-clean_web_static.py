bfb189a430a08b504e799b2f08b64aef05d9ca41
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-11 12:18:22 +0200

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"compress web_stack locally"
-import tarfile
-from fabric.api import local
-from datetime import datetime
+# Fabfile to delete out-of-date archives.
 import os
+from fabric.api import local
 
 
 def do_clean(number=0):
-    "compress"
-    date = datetime.now().strftime('%Y%m%d%H%M%S')
-    # print(os.path.exists("versions"))
-    res = local("ls versions/")
-    print(res)
+    """Delete out-of-date archives.
+    """
+    if int(number) == 0:
+        number = 1
+    else:
+        number = int(number)
+    tot_archives = sorted(os.listdir('versions'))
+    [tot_archives.pop() for i in range(number)]
+    [local("rm versions/{}".format(a)) for a in tot_archives]
