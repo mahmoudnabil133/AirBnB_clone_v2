b5c0d34e7668a0189c636c96ad395977146e1903
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-22 21:02:27 +0200

    num

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,26 +31,10 @@ def python(text=None):
     return f'Python {new_text}'
 
 
-
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    try:
-        num = int(n)
-        return f'{num} is a number'
-    except :
-        html_content = """
-        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-        <html>
-        <head>
-            <title>404 Not Found</title>
-        </head>
-        <body>
-            <h1>Not Found</h1>
-            <p>The requested URL was not found on the server. If you entered the URL manually, please check your spelling and try again.</p>
-        </body>
-        </html>
-        """
-        return html_content
+    return f'{n} is a number'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
