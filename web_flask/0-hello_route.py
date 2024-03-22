0c50e6de8a204d4d69fec358ce83d9d060bf813f
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-22 17:17:00 +0200

    flask setup

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"intro to flask"
+
+from flask import Flask
+
+app = Flask('--name--')
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
