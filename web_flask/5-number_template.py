9c6f7a0099434dc1b0a4047b443270853372cb46
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-22 23:22:29 +0200

    sas

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     "try this"
-    return (render_template('5-number.html', number=int(n)))
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
