1ebda7468770be52d53317f4bd75c8e37954a165
mahmoudnabil133 <hodanabil144@gmail.com>
2024-03-22 21:48:40 +0200

    why error

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', number=int(n))
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
